0bc803ad3c94de3e66366e1cc448d0a743b533fc
Oskar Yildiz <yildizoskar@gmail.com>
2019-10-08 19:56:18 +0200

    Get push token and store in db

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+import Config from 'react-native-config'
+
+export default {
+  API_BASE_URL: Config.RN_API_URL,
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,26 @@
 import React, {Component} from 'react'
 
+import messaging from '@react-native-firebase/messaging'
+import permissions from 'react-native-permissions'
+import axios from 'axios'
+
 import AppNavigator from './navigation'
+import config from './config'
+
+const API_BASE_URL = config.API_BASE_URL
 
 class App extends Component {
+  async componentDidMount() {
+    const {status} = await permissions.checkNotifications()
+    if (status === 'denied') {
+      await permissions.requestNotifications(['alert', 'badge'])
+    }
+    await this.getToken()
+  }
+  async getToken() {
+    const token = await messaging().getToken()
+    await axios.post(`${API_BASE_URL}/push/token`, {token})
+  }
   render() {
     return <AppNavigator />
   }
